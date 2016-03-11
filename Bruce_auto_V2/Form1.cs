57b60ca18dc7172fc73b3b91d56358cee93994d4
blueblance <blueblance@gmail.com>
2016-03-11 18:43:13 +0800

    ver1
    僅有usb_cam功能,將picturebox委派做到MIPI.cs

---
+++ Bruce_auto_V2/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AForge.Video.DirectShow;
+using System.Threading;
+
+namespace Bruce_auto_V2
+{
+    public delegate void Image_Display(Bitmap Image);
+    public partial class Form1 : Form
+    {
+        
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public FilterInfoCollection USB_Webcams = null;
+        public VideoCaptureDevice Cam = null;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (Cam != null)
+            {
+                if (Cam.IsRunning)  // When Form1 closes itself, WebCam must stop, too.
+                {
+                    Cam.Stop();   // WebCam stops capturing images.
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Start")
+            {
+                button1.Text = "Stop";
+                Cam.Start();   // WebCam starts capturing images.
+            }
+            else
+            {
+                button1.Text = "Start";
+                Cam.Stop();  // WebCam stops capturing images.
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            start_getcam();
+
+        }
+
+
+
+        private void Cam_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            //pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+            MIPI mipicam = new MIPI();
+            mipicam.DisplayOnPictureBox(pictureBox1, (Bitmap)eventArgs.Frame.Clone());
+            //DisplayOnPictureBox(pictureBox1, (Bitmap)eventArgs.Frame.Clone());
+
+        }
+        private void start_getcam()
+        {
+            USB_Webcams = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (USB_Webcams.Count > 0)  // The quantity of WebCam must be more than 0.
+            {
+                
+                Cam = new VideoCaptureDevice(USB_Webcams[0].MonikerString);
+                Cam.NewFrame += new AForge.Video.NewFrameEventHandler(Cam_NewFrame);
+                Cam.Start();
+            }
+            else
+            {
+                
+                MessageBox.Show("No video input device is connected.");
+            }
+        }
+    }
+}
---
+++ Bruce_auto_V2/MIPI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Bruce_auto_V2
+{
+    class MIPI
+    {
+        public delegate void DisplayOnPictureBoxcallback(PictureBox pb, Bitmap Bp);
+        public void DisplayOnPictureBox(PictureBox pb, Bitmap Bp)
+        {
+            if (pb.InvokeRequired)
+            {
+                DisplayOnPictureBoxcallback Db = new DisplayOnPictureBoxcallback(DisplayOnPictureBox);
+                pb.BeginInvoke(Db, new object[] { pb, Bp });
+            }
+            else
+            {
+                pb.Image = Bp;
+            }
+        }
+    }
+}
